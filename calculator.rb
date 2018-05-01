c38d8315066ee6ba6b4db2e988623d809ed9a69d
donrestarone <shashikejayatunge@gmail.com>
2018-05-01 13:15:49 -0400

    4 tests passing

---
+++ calculator.rb
@@ -0,0 +1,8 @@
+def add(num_one, num_two)
+	computation = num_one.to_f + num_two.to_f
+	return computation
+end 
+
+def subtract(num_one, num_two)
+	computation = num_one.to_f - num_two.to_f
+end
--- test_calculator.rb
+++ test_calculator.rb
@@ -5,11 +5,13 @@ require './calculator.rb'
 
 class TestCalculator < MiniTest::Test
   # add takes two parameters and adds them
+
   def test_adds_0_and_0
     assert_equal 0, add(0, 0)
   end
 
   def test_adds_2_and_2
+    
     assert_equal 4, add(2, 2)
   end
 
@@ -19,24 +21,29 @@ class TestCalculator < MiniTest::Test
 
  # subtract takes two parameters and subtracts the second from the first
   def test_subtracts_numbers
+    
     assert_equal 6, subtract(10, 4)
   end
 
 # sum takes an *array* of numbers and adds them all together
 # This one is a bit trickier!
   def test_computes_sum_of_empty_array
+    skip
     assert_equal(0, sum([]))
   end
 
   def test_computes_sum_of_single_number
+    skip
     assert_equal 7, sum([7])
   end
 
   def test_computes_sum_of_two_numbers
+    skip
     assert_equal 18, sum([7, 11])
   end
 
   def test_computes_sum_of_many_numbers
+    skip
     assert_equal 25, sum([1,3,5,7,9])
   end
 end
